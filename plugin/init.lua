12aecde50975cf4fc6e3a03151376f9a2292622f
cnrrobertson <cnr.robertson@gmail.com>
2022-10-26 22:34:50 -0400

    Set lua mappings and user commands

--- plugin/init.lua
+++ plugin/init.lua
@@ -2,36 +2,39 @@ local cells = require("nvim-repl-cells")
 local send = require("nvim-repl-cells.send_cells")
 local config = require("nvim-repl-cells.config")
 local mappings = require("nvim-repl-cells.mappings")
+local vim = vim
 -------------------------------------------------------------------------------
 -- Commands
 -------------------------------------------------------------------------------
-vim.api.nvim_create_user_command("CellVisualSelect", function()cells.visual_select_cell(cells.get_marker())end, {})
-vim.api.nvim_create_user_command("CellInsertAbove", function()cells.insert_cell_above(cells.get_marker())end, {})
-vim.api.nvim_create_user_command("CellInsertBelow", function()cells.insert_cell_below(cells.get_marker())end, {})
-vim.api.nvim_create_user_command("CellInsertHere", function()cells.insert_cell_here(cells.get_marker())end, {})
-vim.api.nvim_create_user_command("CellDelete", function()cells.delete_cell(cells.get_marker())end, {})
-vim.api.nvim_create_user_command("CellSplit", function()cells.split_cell(cells.get_marker())end, {})
-vim.api.nvim_create_user_command("CellMergeAbove", function()cells.merge_cell_below(cells.get_marker())end, {})
-vim.api.nvim_create_user_command("CellMergeBelow", function()cells.merge_cell_above(cells.get_marker())end, {})
-vim.api.nvim_create_user_command("CellJumpToNext", function()cells.jump_to_next_cell(cells.get_marker())end, {})
-vim.api.nvim_create_user_command("CellJumpToPrevious", function()cells.jump_to_previous_cell(cells.get_marker())end, {})
-vim.api.nvim_create_user_command("CellToggleFold", function()cells.toggle_cell_fold(cells.get_marker())end, {})
-vim.api.nvim_create_user_command("CellFoldAll", function()cells.fold_all_cells(cells.get_marker())end, {})
-vim.api.nvim_create_user_command("CellUnfoldAll", function()cells.unfold_all_cells(cells.get_marker())end, {})
+vim.api.nvim_create_user_command("CellVisualSelect", function()cells.visual_select_in_cell()end, {})
+vim.api.nvim_create_user_command("CellVisualSelectIn", function()cells.visual_select_in_cell()end, {})
+vim.api.nvim_create_user_command("CellVisualSelectAround", function()cells.visual_select_around_cell()end, {})
+vim.api.nvim_create_user_command("CellInsertAbove", function()cells.insert_cell_above()end, {})
+vim.api.nvim_create_user_command("CellInsertBelow", function()cells.insert_cell_below()end, {})
+vim.api.nvim_create_user_command("CellInsertHere", function()cells.insert_cell_here()end, {})
+vim.api.nvim_create_user_command("CellDelete", function()cells.delete_cell()end, {})
+vim.api.nvim_create_user_command("CellSplit", function()cells.split_cell()end, {})
+vim.api.nvim_create_user_command("CellMergeAbove", function()cells.merge_cell_above()end, {})
+vim.api.nvim_create_user_command("CellMergeBelow", function()cells.merge_cell_below()end, {})
+vim.api.nvim_create_user_command("CellJumpToNext", function()cells.jump_to_next_cell()end, {})
+vim.api.nvim_create_user_command("CellJumpToPrevious", function()cells.jump_to_previous_cell()end, {})
+vim.api.nvim_create_user_command("CellToggleFold", function()cells.toggle_cell_fold()end, {})
+vim.api.nvim_create_user_command("CellFoldAll", function()cells.fold_all_cells()end, {})
+vim.api.nvim_create_user_command("CellUnfoldAll", function()cells.unfold_all_cells()end, {})
 
-vim.api.nvim_create_user_command("CellSendLine", send.send_line, {})
-vim.api.nvim_create_user_command("CellSendVisual", send.send_visual, {})
-vim.api.nvim_create_user_command("CellSend", send.send_cell, {})
-vim.api.nvim_create_user_command("CellSendAndJump", function()send.send_cell();cells.jump_to_next_cell(cells.get_marker())end, {})
-vim.api.nvim_create_user_command("CellSendFile", send.send_file, {})
+vim.api.nvim_create_user_command("CellSendLine", function()send.send_line()end, {})
+vim.api.nvim_create_user_command("CellSendVisual", function()send.send_visual()end, {range=true})
+vim.api.nvim_create_user_command("CellSend", function()send.send_cell()end, {})
+vim.api.nvim_create_user_command("CellSendAndJump", function()send.send_cell();cells.jump_to_next_cell()end, {})
+vim.api.nvim_create_user_command("CellSendFile", function()send.send_file()end, {})
 
 vim.api.nvim_create_user_command("CellPutLine", function()send.put_line(config.cell_register)end, {})
-vim.api.nvim_create_user_command("CellPutVisual", function()send.put_visual(config.cell_register)end, {})
+vim.api.nvim_create_user_command("CellPutVisual", function()send.put_visual(config.cell_register)end, {range=true})
 vim.api.nvim_create_user_command("CellPut", function()send.put_cell(config.cell_register)end, {})
-vim.api.nvim_create_user_command("CellPutAndJump", function()send.put_cell(config.cell_register);cells.jump_to_next_cell(cells.get_marker())end, {})
+vim.api.nvim_create_user_command("CellPutAndJump", function()send.put_cell(config.cell_register);cells.jump_to_next_cell()end, {})
 vim.api.nvim_create_user_command("CellPutFile", function()send.put_file(config.cell_register)end, {})
 
-vim.api.nvim_create_user_command("ToggleBufTerm", send.toggle, {})
+vim.api.nvim_create_user_command("ToggleBufTerm", function()send.toggle()end, {})
 
 -------------------------------------------------------------------------------
 -- Mappings
@@ -47,10 +50,10 @@ vim.api.nvim_create_autocmd({"BufEnter"}, {group="ReplCells",pattern={"*"},callb
 -- Autocommands
 -------------------------------------------------------------------------------
 if config.highlight == true then
-  vim.api.nvim_create_autocmd({"BufWrite","BufEnter"},{group="ReplCells",pattern={"*"},callback=function()cells.highlight_cells(cells.get_marker())end})
+  vim.api.nvim_create_autocmd({"BufWrite","BufEnter"},{group="ReplCells",pattern={"*"},callback=function()cells.highlight_cells()end})
 end
 if config.autofold == true then
-  vim.api.nvim_create_autocmd({"SessionLoadPost","BufReadPost"},{group="ReplCells",pattern={"*"},callback=function()cells.fold_all_cells(cells.get_marker())end})
+  vim.api.nvim_create_autocmd({"SessionLoadPost","BufReadPost"},{group="ReplCells",pattern={"*"},callback=function()cells.fold_all_cells()end})
 end
 if config.foldtext == true then
   cells.set_foldtext()
